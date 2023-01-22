d1c3ff9bd6dc122b32a92791cbba5289e582983f
Jaswanth Sagar Motarugadde <jaswanth.motarugadde2@mail.dcu.ie>
2023-01-22 18:22:57 +0530

    test files

---
+++ hello-rust/target/debug/deps/hello_rust-eaa6cce4100b8ff8.d
@@ -0,0 +1,5 @@
+/Users/jaswanth/rust-classroom/hello-rust/target/debug/deps/hello_rust-eaa6cce4100b8ff8.rmeta: src/main.rs
+
+/Users/jaswanth/rust-classroom/hello-rust/target/debug/deps/hello_rust-eaa6cce4100b8ff8.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/deps/hello_rust-f485e8596e861d72.d
@@ -0,0 +1,5 @@
+/Users/jaswanth/rust-classroom/hello-rust/target/debug/deps/hello_rust-f485e8596e861d72.rmeta: src/main.rs
+
+/Users/jaswanth/rust-classroom/hello-rust/target/debug/deps/hello_rust-f485e8596e861d72.d: src/main.rs
+
+src/main.rs:
